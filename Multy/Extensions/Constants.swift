6855fc9a5ff1e4ef85d3ed32562d6a8b65b485d7
Ivan Apet <van.ka515@gmail.com>
2018-07-25 14:47:29 +0300

    Change Invite qr

--- Multy/Extensions/Constants.swift
+++ Multy/Extensions/Constants.swift
@@ -183,9 +183,9 @@ let minSatoshiToDonate: UInt64          = 5000  //5k minimum sum to donate
 //let apiUrl = "http://88.198.47.112:2278/"//"http://192.168.0.121:7778/"
 
 
-let shortURL = "api.multy.io"
-let apiUrl = "https://\(shortURL)/"
-let socketUrl = "wss://\(shortURL)/"
+let shortURL = "test.multy.io"
+let apiUrl = "http://\(shortURL)/"
+let socketUrl = "ws://\(shortURL)/"
 
 //stage
 //let shortURL = "148.251.42.107/"
@@ -205,4 +205,4 @@ let nonLocalURL = "http://88.198.47.112:7778/"
 let BluetoothSettingsURL_iOS9 = "prefs:root=Bluetooth"
 let BluetoothSettingsURL_iOS10 = "App-Prefs:root=Bluetooth"
 
-let inviteCodeCount = 56
+let inviteCodeCount = 45
