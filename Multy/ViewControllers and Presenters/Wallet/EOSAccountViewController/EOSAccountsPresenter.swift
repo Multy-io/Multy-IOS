8668858ebb006246bb2ad3bd230a736ba017d86e
Pidji <alexeevartsiom@gmail.com>
2018-07-31 12:13:14 +0300

    added eos accounts screen

---
+++ Multy/ViewControllers and Presenters/Wallet/EOSAccountViewController/EOSAccountsPresenter.swift
@@ -0,0 +1,15 @@
+//Copyright 2018 Idealnaya rabota LLC
+//Licensed under Multy.io license.
+//See LICENSE for details
+
+import Foundation
+
+class EOSAccountsPresenter: NSObject {
+    var viewController : EOSAccountsViewController?
+    
+    func presentedViewDidLoad() {
+    }
+    
+    func presentedViewWillAppear() {
+    }
+}
