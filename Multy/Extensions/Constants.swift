c352f126be327ad3d58e0a5a65473665a44742f8
Ivan Apet <van.ka515@gmail.com>
2018-09-27 11:59:27 +0300

    BUILD 61

--- Multy/Extensions/Constants.swift
+++ Multy/Extensions/Constants.swift
@@ -236,7 +236,7 @@ let minSatoshiToDonate: UInt64          = 5000  //5k minimum sum to donate
 //let apiUrl = "http://88.198.47.112:2278/"//"http://192.168.0.121:7778/"
 
 
-let shortURL = "test.multy.io"
+let shortURL = "api.multy.io"
 let apiUrl = "http://\(shortURL)/"
 let socketUrl = "ws://\(shortURL)/"
 
