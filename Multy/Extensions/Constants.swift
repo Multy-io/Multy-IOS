15b731ab4d8d937ef427918b6af1c0eb221639e1
Ivan Apet <van.ka515@gmail.com>
2018-10-15 19:40:25 +0300

    Release build 65

--- Multy/Extensions/Constants.swift
+++ Multy/Extensions/Constants.swift
@@ -247,9 +247,9 @@ let minimumAmountForMakeEthTX = BigInt("\(900_000_000_000_000)") // == 10 cent 1
 //let apiUrl = "https://\(shortURL)/"
 //let socketUrl = "wss://\(shortURL)/"
 
-let shortURL = "test.multy.io"
-let apiUrl = "http://\(shortURL)/"
-let socketUrl = "ws://\(shortURL)/"
+let shortURL = "api.multy.io"
+let apiUrl = "https://\(shortURL)/"
+let socketUrl = "wss://\(shortURL)/"
 
 //stage
 //let shortURL = "148.251.42.107/"
