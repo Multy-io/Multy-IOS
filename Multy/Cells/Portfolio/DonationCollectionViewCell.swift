9868bae5678605cdd0d102758d431699e27aa4c2
Ivan Apet <van.ka515@gmail.com>
2018-10-25 15:22:28 +0300

    Add dragon baner

--- Multy/Cells/Portfolio/DonationCollectionViewCell.swift
+++ Multy/Cells/Portfolio/DonationCollectionViewCell.swift
@@ -11,26 +11,39 @@ class DonationCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var backgroundImg: UIImageView!
     @IBOutlet weak var midLbl: UILabel!
     @IBOutlet weak var botView: UIView!
+    @IBOutlet weak var bannerImg: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.setupUIfor(view: botView)
+        
     }
     
     func makeCellBy(index: Int) {
         switch index {
         case 0:
+            self.bannerImg.isHidden = false
+            self.bannerImg.image = UIImage(named: "multy-dragon-banner382")
+//            self.bannerImg.image = UIImage(named: "grad")
+//            self.bannerImg.layer.cornerRadius = 18
+            self.bannerImg.layer.masksToBounds = true
+//            setupUIfor(view: botView, color: #colorLiteral(red: 0.01194981113, green: 0.3642002213, blue: 0.9994105697, alpha: 0.5985124144))
+            self.setupUIfor(view: botView, color: #colorLiteral(red: 0.6509803922, green: 0.6941176471, blue: 0.7764705882, alpha: 1))
+        case 1:
             self.backgroundImg.image = #imageLiteral(resourceName: "portfolioDonationImage")
             self.midLbl.text = localize(string: Constants.cryptoPortfolioString)
-        case 1:
+            self.bannerImg.isHidden = true
+            self.setupUIfor(view: botView, color: #colorLiteral(red: 0.6509803922, green: 0.6941176471, blue: 0.7764705882, alpha: 0.6))
+        case 2:
             self.backgroundImg.image = #imageLiteral(resourceName: "chartsDonationImage")
             self.midLbl.text = localize(string: Constants.currenciesChartsString)
+            self.bannerImg.isHidden = true
+            self.setupUIfor(view: botView, color: #colorLiteral(red: 0.6509803922, green: 0.6941176471, blue: 0.7764705882, alpha: 0.6))
         default: break
         }
     }
     
-    func setupUIfor(view: UIView) {
-        view.layer.shadowColor = #colorLiteral(red: 0.6509803922, green: 0.6941176471, blue: 0.7764705882, alpha: 0.6)
+    func setupUIfor(view: UIView, color: CGColor ) {
+        view.layer.shadowColor = color
         view.layer.shadowOpacity = 1
         view.layer.shadowOffset = CGSize.zero
         view.layer.shadowRadius = 10
